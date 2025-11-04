0ac50abda3c8e051028657165e0f33c38dc22774
Khalerzaad <khalerzaad@gmail.com>
2025-11-04 18:07:56 +0100

    Adding toggle to Orbits

---
+++ Assets/Scripts/ToggleOrbit.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class ToggleOrbit : MonoBehaviour
+{
+    private Orbits orbit;
+
+    private void Awake()
+    {
+        orbit = GetComponent<Orbits>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Si on appuie espace
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // inverse le statut de orbit
+            orbit.enabled = !orbit.enabled;
+        }
+    }
+}
